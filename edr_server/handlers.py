3e553a194314fcbdcdfc73e0445a395a0b58958f
DPeterK <peter.killick@informaticslab.co.uk>
2022-01-10 11:20:15 +0000

    Generic handler, parameter handling and http error raise

--- edr_server/handlers.py
+++ edr_server/handlers.py
@@ -3,62 +3,144 @@ import json
 from tornado.web import HTTPError, RequestHandler
 
 
+class QueryParameters(RequestHandler):
+    def __init__(self) -> None:
+        self._params_dict = {
+            "coords": None,
+            "datetime": None,
+            "z": None,
+            "parameter-name": None,
+            "f": None,
+            "crs": None,
+        }
+
+    def __getitem__(self, key):
+        return self._params_dict[key]
+
+    def __setitem__(self, key, value):
+        self._params_dict[key] = value
+
+    def keys(self):
+        return self._params_dict.keys()
+
+    def handle_parameters(self, key, values):
+        if not len(values):
+            value = None
+        elif len(values) == 1:
+            value, = values
+        else:
+            emsg = f"Expected exactly 1 parameter for {key!r}, got {len(values)}: {values}."
+            raise ValueError(emsg)
+        if value is not None:
+            meth = getattr(self, f"_handle_{key.replace('-', '_')}")
+            meth(value)
+
+    def _handle_coords(self, value):
+        key = "coords"
+        self[key] = value
+
+    def _handle_crs(self, value):
+        key = "crs"
+        self[key] = value
+
+    def _handle_datetime(self, value):
+        key = "datetime"
+        self[key] = value
+
+    def _handle_f(self, value):
+        """Handle the query parameter `f` - the requested return type for the data."""
+        key = "f"
+        valid_types = ["json", "coveragejson"]
+        if not len(value):
+            value = "json"
+        if value.lower() not in valid_types:
+            HTTPError(415, f"Return type {value!r} is not supported.")
+        self[key] = value
+
+    def _handle_parameter_name(self, value):
+        key = "parameter-name"
+        self[key] = value.split(",")
+
+    def _handle_z(self, value):
+        """Handle the query parameter `z` - vertical coords."""
+        key = "z"
+        self[key] = value
+
+
+class _Handler(RequestHandler):
+    """Generic handler for EDR queries."""
+    def initialize(self):
+        self.query_parameters = QueryParameters()
+
+    def get(self, collection_name):
+        """Handle a get request for data from EDR. Returned as JSON, unless the query specifies otherwise."""
+        self.handle_parameters()
+        self.write(f"Get {self.handler_type} request called for collection {collection_name!r}.")
+
+    def handle_parameters(self):
+        # print(self.get_query_arguments())
+        for key in self.query_parameters.keys():
+            param_vals = self.get_arguments(key)
+            self.query_parameters.handle_parameters(key, param_vals)
+        print(self.query_parameters._params_dict)
+
+
 class AreaHandler(RequestHandler):
     """Handle area requests."""
+    handler_type = "area"
     def get(self, collection_name):
         """Handle a 'get area' request."""
         # Not implemented!
-        HTTPError(501, "Get area request is not implemented.")
+        print("In subclass get...")
+        raise HTTPError(501, f"Get {self.handler_type} request is not implemented.")
 
 
 class CorridorHandler(RequestHandler):
     """Handle corridor requests."""
+    handler_type = "corridor"
     def get(self, collection_name):
         """Handle a 'get corridor' request."""
         # Not implemented!
-        HTTPError(501, "Get corridor request is not implemented.")
+        raise HTTPError(501, f"Get {self.handler_type} request is not implemented.")
 
 
 class CubeHandler(RequestHandler):
     """Handle cube requests."""
+    handler_type = "cube"
     def get(self, collection_name):
         """Handle a 'get cube' request."""
         # Not implemented!
-        HTTPError(501, "Get cube request is not implemented.")
+        raise HTTPError(501, f"Get {self.handler_type} request is not implemented.")
 
 
-class ItemsHandler(RequestHandler):
+class ItemsHandler(_Handler):
     """Handle items requests."""
-    def get(self, collection_name):
-        """Handle a 'get items' request."""
-        self.write(f"Get items request called for collection {collection_name!r}.")
+    handler_type = "items"
 
 
-class LocationsHandler(RequestHandler):
+class LocationsHandler(_Handler):
     """Handle location requests."""
-    def get(self, collection_name):
-        """Handle a 'get locations' request."""
-        self.write(f"Get locations request called for collection {collection_name!r}.")
+    handler_type = "location"
 
 
-class PositionHandler(RequestHandler):
+class PositionHandler(_Handler):
     """Handle position requests."""
-    def get(self, collection_name):
-        """Handle a 'get position' request."""
-        self.write(f"Get position request called for collection {collection_name!r}.")
+    handler_type = "position"
 
 
 class RadiusHandler(RequestHandler):
     """Handle radius requests."""
+    handler_type = "radius"
     def get(self, collection_name):
         """Handle a 'get radius' request."""
         # Not implemented!
-        HTTPError(501, "Get radius request is not implemented.")
+        raise HTTPError(501, f"Get {self.handler_type} request is not implemented.")
 
 
 class TrajectoryHandler(RequestHandler):
     """Handle trajectory requests."""
+    handler_type = "trajectory"
     def get(self, collection_name):
         """Handle a 'get trajectory' request."""
         # Not implemented!
-        HTTPError(501, "Get trajectory request is not implemented.")
+        raise HTTPError(501, f"Get {self.handler_type} request is not implemented.")
